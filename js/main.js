8a647c12c7342d130e46ccac5260f9cdc16f8f24
Oleg <slame1121@gmail.com>
2018-03-29 23:30:52 +0300

    Header catalog

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 var Main = {
 
 
+	initCatalogDropDown: function () {
+
+		var fixed_navbar = $('.fixed_navbar');
+		fixed_navbar.find('.categories_block_container-item:not(.active)').hide();
+		//navbar
+		$('.fixed_navbar_container-item__catalog-dropdown__catalog-navbar').on('mouseover', 'ul li', function(){
+			$(this).parent().find('a').css('font-weight', '');
+			$(this).find('a').css('font-weight','bold');
+		}).on('mouseout','ul li', function(){
+			$(this).parent().find('a').css('font-weight', '');
+		}).on('click','ul li a', function(e){
+			e.stopPropagation();
+			e.preventDefault();
+
+			$(this).closest('ul').find('li').removeClass('active');
+			$(this).closest('li').addClass('active');
+			fixed_navbar.find('.categories_block_container-item').hide().removeClass('active');
+			var self = $(this);
+			var category_id = self.data('category-id');
+			fixed_navbar.find('.categories_block_container-item[data-subcategory='+category_id+']').show().addClass('active');
+
+		});
+	},
 	init: function(){
 		this.initProductsList();
+
+		this.initCatalogDropDown();
 	},
 	initProductsList: function(){
 		var owl = $('#product_banner');
